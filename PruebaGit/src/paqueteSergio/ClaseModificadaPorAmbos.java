93659dd02c819743a4aeb1a37b51c5906e59b229
sergio <sergio@ROG>
2016-04-07 11:29:23 +0200

    Hola2

---
+++ PruebaGit/src/paqueteSergio/ClaseModificadaPorAmbos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteSergio;
+
+/**
+ *
+ * @author sergio
+ */
+public class ClaseModificadaPorAmbos {
+    
+    public void hola2()
+    {
+        System.out.println("Hola2");
+    }
+    
+}
