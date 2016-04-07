d1bf3a08568c19e8ca902ba19b81db46c557ccc1
MªPilar <MPilar@Pilar>
2016-04-07 11:11:31 +0200

    Añadimos ClasePilar

--- PruebaGit/src/paquetePilar/ClasePilar.java
+++ PruebaGit/src/paquetePilar/ClasePilar.java
@@ -13,7 +13,7 @@ package paquetePilar;
 public class ClasePilar {
     public static void main(String[] args)
     {
-        System.out.print("Hola Mundo");
+        System.out.print("Hola Mundo Feliz");
     }
     
 }
